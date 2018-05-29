e4696a7d81c928fbef05475033d88a97f3c489bf
Anna P <abbessk@gmail.com>
2018-05-29 18:43:07 -0500

    hooo

--- TemplateData/UnityProgress.js
+++ TemplateData/UnityProgress.js
@@ -1,11 +1,11 @@
 function UnityProgress(gameInstance, progress) {
   if (!gameInstance.Module)
     return;
-  if (!gameInstance.logo) {
-    // gameInstance.logo = document.createElement("div");
-    // gameInstance.logo.className = "logo " + gameInstance.Module.splashScreenStyle;
-    // gameInstance.container.appendChild(gameInstance.logo);
-  }
+  // if (!gameInstance.logo) {
+  //   gameInstance.logo = document.createElement("div");
+  //   gameInstance.logo.className = "logo " + gameInstance.Module.splashScreenStyle;
+  //   gameInstance.container.appendChild(gameInstance.logo);
+  // }
   if (!gameInstance.progress) {    
     gameInstance.progress = document.createElement("div");
     gameInstance.progress.className = "progress " + gameInstance.Module.splashScreenStyle;
@@ -16,11 +16,11 @@ function UnityProgress(gameInstance, progress) {
     gameInstance.progress.full.className = "full";
     gameInstance.progress.appendChild(gameInstance.progress.full);
     gameInstance.container.appendChild(gameInstance.progress);
-
   }
   gameInstance.progress.full.style.width = (100 * progress) + "%";
   gameInstance.progress.empty.style.width = (100 * (1 - progress)) + "%";
-  if (progress == 1)
-    gameInstance.animate.style.display = gameInstance.progress.style.display = "none";
+  if (progress == 1) {
+    // gameInstance.logo.style.display = gameInstance.progress.style.display = "none";
+    gameInsta.animate.style.display = "none";
+  }
 }
-
